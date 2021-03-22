de973c6042c786ce8375975c8d3373d499ca1775
EC2 Default User <ec2-user@ip-172-31-74-173.ec2.internal>
2021-03-22 13:24:01 +0000

    [finish]2-3

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,22 @@
+class TodolistsController < ApplicationController
+  def new
+    # viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    @list = List.new
+  end
+  
+  def create
+    # 1.データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 2.データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 3.トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+  
+end
